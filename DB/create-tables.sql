e9fc8b0fd674b8d3c17c3e2f7fee31795c6f0768
Nayla Portas <naylaportas@gmail.com>
2020-11-07 21:43:36 -0300

    Adds a new student as an example

--- DB/create-tables.sql
+++ DB/create-tables.sql
@@ -16,4 +16,7 @@ PRIMARY KEY (id)
 INSERT INTO student(name, email, career, birth_date, phone_number, country, city)
 VALUES("Violeta Gonzalez", "violeta@gmail.com", "Data science", "1984-07-04", "+541161507899", "Argentina", "CABA");
 
+INSERT INTO student(name, email, career, birth_date, phone_number, country, city)
+VALUES("Santiago Perez", "santiperez@gmail.com", "Data science", "1995-12-02", "+541166989984", "Argentina", "CABA");
+
   
