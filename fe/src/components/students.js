b2772abb9d9243b15bdfca26d72d12bde8f13c68
Nayla Portas <naylaportas@gmail.com>
2020-11-08 12:25:18 -0300

    Adds in the students list the payment option

--- fe/src/components/students.js
+++ fe/src/components/students.js
@@ -11,8 +11,9 @@ import React from 'react'
                 <h6 className="card-subtitle mb-2 text-muted">{student.email}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">{student.phone_number}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">{student.country}, {student.city}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Date of birth: {student.birth_date}</h6>
                 <p className="card-text">Career: {student.career}</p>
-                <p className="card-text">Date of birth: {student.birth_date}</p>
+                <p className="card-text">Payment option: {student.payment_option}</p>
             </div>
             </div>
         ))}
