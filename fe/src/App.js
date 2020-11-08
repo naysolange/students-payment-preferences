551183980658161b5d9b2fff86826676b301a31f
Nayla Portas <naylaportas@gmail.com>
2020-11-07 21:45:05 -0300

    Creates a page with all the students

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Students from './components/students';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+   render() {
+        return (
+            <Students students={this.state.students} />
+        )
+    }
+
+    state = {
+        students: []
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:8080/students')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ students: data })
+            })
+            .catch(console.log)
+    }
 }
 
 export default App;
---
+++ fe/src/components/students.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+    const Students = ({ students }) => {
+      return (
+        <div>
+          <center><h1>Students Payment Preferences</h1></center>
+          {students.map((student) => (
+            <div className="card" key={student.id}>
+                <div className="card-body">
+                <h5 className="card-title">{student.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{student.email}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{student.phone_number}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{student.country}, {student.city}</h6>
+                <p className="card-text">Career: {student.career}</p>
+                <p className="card-text">Date of birth: {student.birth_date}</p>
+            </div>
+            </div>
+        ))}
+        </div>
+         
+      )
+    };
+
+export default Students
