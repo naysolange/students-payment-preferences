874fe720d53194256ec797d37325074f2f6b1a98
Nayla Portas <naylaportas@gmail.com>
2020-11-10 19:49:43 -0300

    Adds links to Home and Create Student

--- fe/src/components/header.js
+++ fe/src/components/header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
+
     const Header = () => {
       return (
-        <div className="Title">
-        <header>
-          <h1>Students Payment Preferences</h1>
-        </header>
+        <div>
+            <div className="Title">
+                <header>
+                <h1>Students Payment Preferences</h1>
+                <div className="menu">
+                    <a href="/">Home</a>
+                    <a href="/create">Create Student</a>
+                </div>
+                </header>
+        </div>
       </div>
       )
     };
