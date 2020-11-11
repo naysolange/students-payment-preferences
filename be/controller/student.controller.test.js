3b7e6d79b9bf5bd51a8438de63c2e63e1938cbdf
Nayla Portas <naylaportas@gmail.com>
2020-11-11 17:30:31 -0300

    Adds dependencies for testing

---
+++ be/controller/student.controller.test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('Student Controller', function() {
+    describe('#findAll()', function() {
+      it.only('test dependencies', function() {
+        assert.isTrue(true);
+      });
+    });
+  });
